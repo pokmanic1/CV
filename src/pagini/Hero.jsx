70f4ab95dfa078c53c126696a404190cf6f1c116
cirjeudumitru-prog <cirjeu.dumitru@elev.cihcahul.md>
2026-07-01 07:28:51 +0300

    Sectiunea despre mine finalizata

--- src/pagini/Hero.jsx
+++ src/pagini/Hero.jsx
@@ -12,7 +12,18 @@ const Hero = () => {
 
                     <div className='w-full h-full p-[20px] md:col-span-2  border border-black  '>
                         <div>
+                            <div>
+                                <h1 className='text-center  text-gradient text-[30px]  leading-tight mb-2'>
+                                    Despre Mine
+                                </h1>
 
+                                <p className='block text-center text-gray-600 text-[16px]'>Dezvoltator Front-End pasionat de web development și de propunerea  ideilor în proiecte practice. Sunt în căutarea unei oportunități într-o echipă dinamică, unde să-mi extind cunoștințele, să învăț de la cei din jurul meu ,contribuind activ la dezvoltarea proiectelor.</p>
+                            </div>
+                        </div>
+                        <div className='col-center gap-[15px] mt-[29px] '>
+                            <div className='border-gradient py-1 px-6 text-center'>Educatie: Colegiul “I. Hajdeu” Cahul</div>
+                            <div className='border-gradient py-1 px-6 text-center'>Varsta: 19 ani</div>
+                            <div className='border-gradient py-1 px-6 text-center'>Dorinta Principala: Invatare -Crestere</div>
                         </div>
                     </div>
 
@@ -22,13 +33,13 @@ const Hero = () => {
                     <div className='w-full md:col-start-3 md:col-span-3 relative z-10'>
 
 
-                        <div className='absolute inset-x-0 -top-10 -bottom-6 gradient-secundar rounded-3xl col-center gap-[10px] px-[10px] '>
+                        <div className='absolute inset-x-0 -top-10 -bottom-6 gradient-secundar rounded-3xl col-center  shadow-[0px_10px_30px_10px_#4b5563] gap-[10px] px-[10px] '>
 
 
 
 
                             <div className='  w-full h-[50%] flex items-end justify-center'>
-                                <img src="/public/IMG-20260220-WA0000.jpg" className='h-[90%] w-[40%] bg-center object-cover rounded-full  border border-green-400' alt="" />
+                                <img src="/public/IMG-20260220-WA0000.jpg" className='h-[90%] w-[40%] bg-center object-cover rounded-full  ' alt="" />
                             </div>
 
 
@@ -55,7 +66,7 @@ const Hero = () => {
                                 </a>
 
                                 <p className='text-center text-white text-[18px] leading-tight'>
-                                    Loc: Chișinău, Moldova
+                                    Locuinta: Cahul, Moldova
                                 </p>
                             </div>
 
